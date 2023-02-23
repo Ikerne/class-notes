74e5a4a429bc2ecd0faf95004684ebdf29de09a1
Ikernee <ikernesanz@gmail.com>
2023-02-23 15:18:58 +0100

    lab solvede

--- Labs/03-JavaScript/04-lab-javascript-greatest-movies/src/movies.js
+++ Labs/03-JavaScript/04-lab-javascript-greatest-movies/src/movies.js
@@ -11,11 +11,7 @@ function getAllDirectors(moviesArray) {
   };
   console.log(getAllDirectors(movies));
 
-  //second option 
 
-//   function getAllDirectors(moviesArray){
-//     return moviesArray.map((movie) => movie.director);
-//    }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {
@@ -25,7 +21,7 @@ function howManyMovies(moviesArray) {
     return filteredMovies.length;
 }
 
-// Iteration 3: All scores average - Get the average of all scores with 2 decimals
+// Iteration 3: All scores average - Get average 2 decimals
 function scoresAverage(moviesArray) {
     if (!moviesArray.length){
         return 0;
@@ -39,11 +35,11 @@ function scoresAverage(moviesArray) {
     }
 }
     let scoresAverage = scores.reduce((a, b) =>  a + b)/scores.length;
-    // console.log(scores);
+
     return Number(scoresAverage.toFixed(2));
 }
 
-//Second shorter version
+//Second short version
 
     const scoresAverage2 = (moviesArray) => {
         if (!movies.length) return 0;
@@ -65,14 +61,10 @@ function dramaMoviesScore(moviesArray) {
 }
 console.log(dramaMoviesScore(movies))
 
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-//WORKS WITHOUT TITLEORDER
-// function orderByYear(moviesArray) {
-//     let sortedByYear = moviesArray.slice(0).sort((one, two) => one.year > two.year ? 1 : -1);
- //     return sortedByYear;
-// }
+// Iteration 5: Ordering by year - Order by year, ascending 
 
-//Con sort the title si el año falla
+
+//With sort the title if the year fails
 function orderByYear(moviesArray) {
 let sortedByYear = moviesArray.slice(0).sort((one, two) => {
     if (one.year > two.year) return one.year - two.year
@@ -90,85 +82,4 @@ function orderAlphabetically(moviesArray) {
     return sortByTitle.slice(0,20);
 }
 console.log(orderAlphabetically(movies))
-// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-// function turnHoursToMinutes(moviesArray) {
-    // let movieHoursToMinutes = moviesArray.map((movie) => movie.duration.substring(0, 1)*60);
-    // let minsIsolated = moviesArray.map((movie) => movie.duration.substring(3,5).replace(/[a-z]+/g, ""));
-    // let sumMinsAndHs = movieHoursToMinutes.map((num, index) => {
-    //     return num + Number(minsIsolated[index]);
-    // })
-    // return sumMinsAndHs;}
-
-    //SOLUTION
-
-   //Part 001
-const convertHours = (hourString) => {
-    let calculateHour = hourString.split("h");
-    return calculateHour[0] * 60;
-  };
-  // Part 002
-  const convertMinutes = (minuteString) => {
-    let calculateMinutes = minuteString.split("min");
-    return Number(calculateMinutes[0]);
-  };
-  // Part 003
-  const convertDuration = (duration) => {
-    let timePieces = duration.split(" ");
-  
-    let minutes = timePieces.reduce((sum, onePiece) => {
-      if (onePiece.includes("h")) {
-        return sum + convertHours(onePiece);
-      }
-      return sum + convertMinutes(onePiece);
-    }, 0);
-  
-    return minutes;
-  };
-  // Part 004
-  const turnHoursToMinutes = (movies) => {
-    let newCentArray = movies.map((movie) => {
-      let newMovie = {};
-      newMovie.title = movie.title;
-      newMovie.year = movie.year;
-      newMovie.director = movie.director;
-      newMovie.duration = convertDuration(movie.duration);
-      newMovie.genre = movie.genre;
-      newMovie.rate = movie.rate;
-  
-      return newMovie;
-    });
-  
-    return newCentArray;
-  };
-
-console.log(turnHoursToMinutes(movies));
-//duration: '2h 22min',
-
-
-
-
-// BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(movies) {
-    if (!movies.length) return null;
-
-    let masterObject = {};
-  
-    movies.forEach((movie) => {
-      if (!masterObject[movie.year]) {
-        masterObject[movie.year] = [movie];
-      } else {
-        masterObject[movie.year].push(movie);
-      }
-    });
-  
-    let highest = 0;
-    let theActualYear;
-    for (let theYear in masterObject) {
-      if (scoresAverage(masterObject[theYear]) > highest) {
-        highest = scoresAverage(masterObject[theYear]);
-        theActualYear = theYear;
-      }
-    }
-    return `The best year was ${theActualYear} with an average score of ${highest}`;
-  };
 
